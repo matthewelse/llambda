8b08c83c162d3a6c8e41b256efa3864a9ea838c9
Matthew Else <759697+matthewelse@users.noreply.github.com>
2021-02-03 18:35:28 +0000

    binary tool

---
+++ bin/llambda.ml
@@ -0,0 +1,16 @@
+open Core
+
+let command =
+  let open Command.Let_syntax in
+  Command.basic
+    ~summary:"Try to compile an OCaml file to LLVM IR"
+    [%map_open
+      let filename = anon ("filename" %: string)
+      and dcmm = flag "-dcmm" no_arg ~doc:"dump c-- IR" in
+      fun () ->
+        let source_code = In_channel.read_all filename in
+        let cmm = Llambda.Trycmm.cmm_of_source ~dump_cmm:dcmm source_code in
+        Llambda.Emit_llvm.emit cmm]
+;;
+
+let () = Command.run command
