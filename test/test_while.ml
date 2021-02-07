d6e53da38faf15ab5324572c52b52338b8f5aa78
Matthew Else <759697+matthewelse@users.noreply.github.com>
2021-02-07 15:46:11 +0000

    correct test_while.ml

--- test/test_while.ml
+++ test/test_while.ml
@@ -25,8 +25,70 @@ let%expect_test "while example" =
     {|
     ("declaring function" (cfundecl.fun_name camlMelse__f_80))
     ("declaring function" (cfundecl.fun_name camlMelse__entry))
+    (codegen_expr
+     (expr
+       "(let_mut x/83: int 1\
+      \n  (catch\
+      \n    (catch rec (exit 2) with(2)\
+      \n      (if (> 21 x/83) (assign x/83 (+ x/83 2)) (exit 1)) (exit 2))\
+      \n  with(1) []) x/83)"))
+    (codegen_expr (expr 1))
+    (codegen_expr
+     (expr
+       "(seq\
+      \n  (catch\
+      \n    (catch rec (exit 2) with(2)\
+      \n      (if (> 21 x/83) (assign x/83 (+ x/83 2)) (exit 1)) (exit 2))\
+      \n  with(1) []) x/83)"))
+    (codegen_expr
+     (expr
+       "(catch\
+      \n  (catch rec (exit 2) with(2)\
+      \n    (if (> 21 x/83) (assign x/83 (+ x/83 2)) (exit 1)) (exit 2))\
+      \nwith(1) [])"))
+    (codegen_expr (expr []))
+    (codegen_expr
+     (expr
+       "(catch rec (exit 2) with(2)\
+      \n  (if (> 21 x/83) (assign x/83 (+ x/83 2)) (exit 1)) (exit 2))"))
+    (codegen_expr
+     (expr "(seq (if (> 21 x/83) (assign x/83 (+ x/83 2)) (exit 1)) (exit 2))"))
+    (codegen_expr (expr "(if (> 21 x/83) (assign x/83 (+ x/83 2)) (exit 1))"))
+    (codegen_expr (expr "(> 21 x/83)"))
+    (codegen_expr (expr 21))
+    (codegen_expr (expr x/83))
+    (codegen_expr (expr "(assign x/83 (+ x/83 2))"))
+    (codegen_expr (expr "(+ x/83 2)"))
+    (codegen_expr (expr x/83))
+    (codegen_expr (expr 2))
+    (codegen_expr (expr "(exit 1)"))
+    (codegen_expr (expr "(exit 2)"))
+    (codegen_expr (expr "(exit 2)"))
+    (codegen_expr (expr x/83))
+    ("finished codegen_expr" (cfundecl.fun_name camlMelse__f_80))
+    (codegen_expr
+     (expr
+      "(seq (let f/80 \"camlMelse__3\" (store val(root-init) \"camlMelse\" f/80)) 1a)"))
+    (codegen_expr
+     (expr
+      "(let f/80 \"camlMelse__3\" (store val(root-init) \"camlMelse\" f/80))"))
+    (codegen_expr (expr "\"camlMelse__3\""))
+    ("Reading const symbol" (name camlMelse__3)
+     (ptr
+      "@camlMelse__3 = global i8* bitcast (i64 (i8*)** getelementptr inbounds ({ i64, i64 (i8*)*, i64 }, { i64, i64 (i8*)*, i64 }* @0, i32 0, i32 1) to i8*)"))
+    "did we get here?"
+    (codegen_expr (expr "(store val(root-init) \"camlMelse\" f/80)"))
+    (codegen_expr (expr "\"camlMelse\""))
+    ("Reading const symbol" (name camlMelse)
+     (ptr
+      "@camlMelse = global i8* bitcast (i64* getelementptr inbounds ({ i64, i64 }, { i64, i64 }* @1, i32 0, i32 1) to i8*)"))
+    "did we get here?"
+    (codegen_expr (expr f/80))
+    (codegen_expr (expr 1a))
+    ("finished codegen_expr" (cfundecl.fun_name camlMelse__entry))
     ; ModuleID = 'melse'
     source_filename = "melse"
+    target triple = "x86_64-apple-macosx10.15.0"
 
     @0 = global { i64, i64 (i8*)*, i64 } { i64 3063, i64 (i8*)* @camlMelse__f_80, i64 3 }
     @camlMelse__3 = global i8* bitcast (i64 (i8*)** getelementptr inbounds ({ i64, i64 (i8*)*, i64 }, { i64, i64 (i8*)*, i64 }* @0, i32 0, i32 1) to i8*)
@@ -75,10 +137,10 @@ let%expect_test "while example" =
 
     define ghccc i8* @camlMelse__entry() {
     entry:
-      %0 = alloca i8**
-      store i8** @camlMelse__3, i8*** %0
-      %1 = load i8**, i8*** %0
-      store i8* bitcast (i8** @camlMelse to i8*), i8** %1
+      %0 = alloca i8*
+      store i8* bitcast (i8** @camlMelse__3 to i8*), i8** %0
+      %1 = load i8*, i8** %0
+      store i8* %1, i8** @camlMelse
       ret i8* inttoptr (i64 1 to i8*)
     }
 
