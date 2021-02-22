fa649848d8fb0f3e38cb82c88e98eb3617631cc0
Matthew Else <759697+matthewelse@users.noreply.github.com>
2021-02-22 19:27:17 +0000

    update more tests for unique vars

--- test/test_examples.ml
+++ test/test_examples.ml
@@ -20,38 +20,38 @@ let%expect_test "post-order visit" =
       ;;];
   [%expect
     {|
-    define ocamlcc i8* @camlTest__postorder_XXX(i8* %f, i8* %t) gc "ocaml" {
+    define ocamlcc i8* @camlTest__postorder_XXX(i8* %f_92, i8* %t_91) gc "ocaml" {
     entry:
-      %0 = getelementptr i8, i8* %t, i64 -8
+      %0 = getelementptr i8, i8* %t_91, i64 -8
       %1 = load i8, i8* %0, align 1
       %zext = zext i8 %1 to i64
       %2 = trunc i64 %zext to i1
       br i1 %2, label %then, label %else
 
     then:                                             ; preds = %entry
-      %3 = getelementptr i8, i8* %t, i64 8
+      %3 = getelementptr i8, i8* %t_91, i64 8
       %load = bitcast i8* %3 to i8**
       %4 = load i8*, i8** %load, align 8
-      %5 = call ocamlcc i8* @camlTest__postorder_XXX(i8* %f, i8* %4)
-      %6 = getelementptr i8, i8* %t, i64 16
+      %5 = call ocamlcc i8* @camlTest__postorder_XXX(i8* %f_92, i8* %4)
+      %6 = getelementptr i8, i8* %t_91, i64 16
       %load1 = bitcast i8* %6 to i8**
       %7 = load i8*, i8** %load1, align 8
-      %8 = call ocamlcc i8* @camlTest__postorder_XXX(i8* %f, i8* %7)
-      %load2 = bitcast i8* %f to i8**
+      %8 = call ocamlcc i8* @camlTest__postorder_XXX(i8* %f_92, i8* %7)
+      %load2 = bitcast i8* %f_92 to i8**
       %9 = load i8*, i8** %load2, align 8
-      %load3 = bitcast i8* %t to i8**
+      %load3 = bitcast i8* %t_91 to i8**
       %10 = load i8*, i8** %load3, align 8
       %func_cast = bitcast i8* %9 to i8* (i8*, i8*)*
-      %11 = call ocamlcc i8* %func_cast(i8* %10, i8* %f)
+      %11 = call ocamlcc i8* %func_cast(i8* %10, i8* %f_92)
       br label %merge
 
     else:                                             ; preds = %entry
-      %load4 = bitcast i8* %f to i8**
+      %load4 = bitcast i8* %f_92 to i8**
       %12 = load i8*, i8** %load4, align 8
-      %load5 = bitcast i8* %t to i8**
+      %load5 = bitcast i8* %t_91 to i8**
       %13 = load i8*, i8** %load5, align 8
       %func_cast6 = bitcast i8* %12 to i8* (i8*, i8*)*
-      %14 = call ocamlcc i8* %func_cast6(i8* %13, i8* %f)
+      %14 = call ocamlcc i8* %func_cast6(i8* %13, i8* %f_92)
       br label %merge
 
     merge:                                            ; preds = %else, %then
@@ -61,5 +61,11 @@ let%expect_test "post-order visit" =
 ;;
 
 let%expect_test "hello world" =
-  runtest ~show_functions:[ "f" ] [%str let f () = print_endline "hello, world!"]
+  runtest ~show_functions:[ "f" ] [%str let f () = print_endline "hello, world!"];
+  [%expect {|
+    define ocamlcc i8* @camlTest__f_XXX(i8* %param_85) gc "ocaml" {
+    entry:
+      %0 = call ocamlcc i8* bitcast (i8* @camlStdlib__print_endline_1181 to i8* (i8*)*)(i8* @camlTest__const_immstring_856)
+      ret i8* %0
+    } |}]
 ;;
