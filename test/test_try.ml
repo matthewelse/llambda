235cb715e4a248eb840b06af38c8712f87d8ff0e
Matthew Else <759697+matthewelse@users.noreply.github.com>
2021-03-08 21:30:34 +0000

    update tests for working-ish try...with blocks

--- test/test_try.ml
+++ test/test_try.ml
@@ -18,61 +18,57 @@ let%expect_test "try/with" =
     {|
     define ocamlcc i8* @camlTest__f_XXX(i8* %n_108, i8* %m_107) gc "ocaml" {
     entry:
-      %read_r15 = call i64 @llvm.read_register.i64(metadata !0)
-      %0 = inttoptr i64 %read_r15 to i8*
+      %read_r14 = call i64 @llvm.read_register.i64(metadata !0)
+      %0 = inttoptr i64 %read_r14 to i8*
       %domain_exn_ptr = getelementptr inbounds i8, i8* %0, i64 16
-      %prev_stack = call i8* @llvm.stacksave()
-      %handler = alloca i8*, align 8
-      %1 = bitcast i8** %handler to i64*
-      store i64 251722446, i64* %1, align 4
-      %old_handler = alloca i8*, align 8
-      store i8* %domain_exn_ptr, i8** %old_handler, align 8
-      %result = call ocamlcc i8* @_llambda_setjmp(i8** %handler, i8* %domain_exn_ptr)
-      %exception_was_raised = icmp eq i8* %result, null
       %result_109 = alloca i8*, align 8
       call void @llvm.gcroot(i8** %result_109, i8* null)
-      br i1 %exception_was_raised, label %body, label %handler1
+      callbr void asm sideeffect "lea $1(%rip),%r11; push %r11; push ($0); mov %rsp,($0)", "r,X,~{r11}"(i8* %domain_exn_ptr, i8* blockaddress(@camlTest__f_XXX, %handler))
+              to label %body [label %handler]
 
     body:                                             ; preds = %entry
-      %2 = call ocamlcc i8* @camlTest__g_20(i8* %n_108)
+      %1 = call ocamlcc i8* @camlTest__g_20(i8* %n_108)
+      call void asm sideeffect "pop ($0); add $$8,%rsp", "r"(i8* %domain_exn_ptr)
       br label %merge
 
-    handler1:                                         ; preds = %entry
-      call void @llvm.stackrestore(i8* %prev_stack)
-      %load = bitcast i8* %result to i8**
-      %3 = load i8*, i8** %load, align 8
-      %4 = ptrtoint i8* %3 to i64
-      %icmp = icmp eq i64 %4, ptrtoint (i8* @camlTest__Pmakeblock_70 to i64)
+    handler:                                          ; preds = %entry
+      %exn = call i8* asm "", "={rax}"()
+      %load = bitcast i8* %exn to i8**
+      %2 = load i8*, i8** %load, align 8
+      %3 = ptrtoint i8* %2 to i64
+      %icmp = icmp eq i64 %3, ptrtoint (i8* @camlTest__Pmakeblock_70 to i64)
       %zext = zext i1 %icmp to i64
-      %5 = trunc i64 %zext to i1
-      br i1 %5, label %then, label %else
+      %4 = trunc i64 %zext to i1
+      br i1 %4, label %then, label %else
 
-    merge:                                            ; preds = %merge3, %body
-      %phi = phi i8* [ %iftmp, %merge3 ], [ %2, %body ]
-      call void @llvm.stackrestore(i8* %prev_stack)
+    merge:                                            ; preds = %merge4, %body
+      %phi = phi i8* [ %iftmp, %merge4 ], [ %1, %body ]
       store i8* %phi, i8** %result_109, align 8
-      %6 = ptrtoint i8* %m_107 to i64
-      %binop = add i64 %6, -1
-      %7 = load i8*, i8** %result_109, align 8
-      %8 = ptrtoint i8* %7 to i64
-      %binop4 = ashr i64 %8, 1
-      %binop5 = mul i64 %binop, %binop4
-      %binop6 = add i64 %binop5, 1
-      %promote = inttoptr i64 %binop6 to i8*
+      %5 = ptrtoint i8* %m_107 to i64
+      %binop = add i64 %5, -1
+      %6 = load i8*, i8** %result_109, align 8
+      %7 = ptrtoint i8* %6 to i64
+      %binop5 = ashr i64 %7, 1
+      %binop6 = mul i64 %binop, %binop5
+      %binop7 = add i64 %binop6, 1
+      %promote = inttoptr i64 %binop7 to i8*
       ret i8* %promote
 
-    then:                                             ; preds = %handler1
-      %9 = getelementptr i8, i8* %result, i64 8
-      %load2 = bitcast i8* %9 to i8**
-      %10 = load i8*, i8** %load2, align 8
-      br label %merge3
+    then:                                             ; preds = %handler
+      %8 = getelementptr i8, i8* %exn, i64 8
+      %load1 = bitcast i8* %8 to i8**
+      %9 = load i8*, i8** %load1, align 8
+      br label %merge4
 
-    else:                                             ; preds = %handler1
-      call ocamlcc void @_llambda_raise_exn(i8* %result)
+    else:                                             ; preds = %handler
+      %read_r142 = call i64 @llvm.read_register.i64(metadata !0)
+      %10 = inttoptr i64 %read_r142 to i8*
+      %domain_exn_ptr3 = getelementptr inbounds i8, i8* %10, i64 16
+      call ocamlcc void asm sideeffect "movq ($1),%rsp; popq ($1); popq %r11; jmp *%r11", "{rax},r"(i8* %exn, i8* %domain_exn_ptr3)
       unreachable
 
-    merge3:                                           ; preds = %then
-      %iftmp = phi i8* [ %10, %then ]
+    merge4:                                           ; preds = %then
+      %iftmp = phi i8* [ %9, %then ]
       br label %merge
     }
 
@@ -85,12 +81,15 @@ let%expect_test "try/with" =
       br i1 %1, label %then, label %else
 
     then:                                             ; preds = %entry
-      call ocamlcc void @_llambda_raise_exn(i8* @camlTest__simplify_fv_90)
+      %read_r14 = call i64 @llvm.read_register.i64(metadata !0)
+      %2 = inttoptr i64 %read_r14 to i8*
+      %domain_exn_ptr = getelementptr inbounds i8, i8* %2, i64 16
+      call ocamlcc void asm sideeffect "movq ($1),%rsp; popq ($1); popq %r11; jmp *%r11", "{rax},r"(i8* @camlTest__simplify_fv_90, i8* %domain_exn_ptr)
       unreachable
 
     else:                                             ; preds = %entry
-      %2 = ptrtoint i8* %x_98 to i64
-      %binop = mul i64 %2, 10
+      %3 = ptrtoint i8* %x_98 to i64
+      %binop = mul i64 %3, 10
       %binop1 = add i64 %binop, -9
       br label %merge
 
